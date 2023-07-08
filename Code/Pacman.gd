9da163ba2630634df1764e575d0b4cedf93a6c47
vlad <linkevichvv@mail.ru>
2023-07-08 19:29:38 +0300

    collect point

--- Code/Pacman.gd
+++ Code/Pacman.gd
@@ -2,6 +2,7 @@ class_name Pacman
 extends AnimatedSprite2D
 
 const BORDER : int = 0
+const POINT : int = 1
 const START_POSITION : Vector2i = Vector2i(10, 11)
 
 const DIRECTION = {
@@ -19,8 +20,10 @@ const DIRECTION_ANIMATION = {
 }
 
 @export var speed : float
+@export var coins_sound : Array[AudioStream]
 
 @onready var tile_map : TileMap = $"../TileMap"
+@onready var audio : AudioStreamPlayer2D = $Audio
 
 var _pos : Vector2i
 var _tween : Tween
@@ -46,15 +49,25 @@ func _check_position(direction : GameInput.Direction) -> bool:
 		
 	var pos : Vector2i = _pos + DIRECTION[direction];
 	return tile_map.get_cell_tile_data(BORDER, pos) == null
-		
+
 
 func _move_to_direction(direction : GameInput.Direction):
 	_pos += DIRECTION[direction];
 	self.play(DIRECTION_ANIMATION[direction])
+	
 	_tween = create_tween()
 	_tween.tween_property(self, ^"position", 2 * tile_map.map_to_local(_pos), 1.0 / speed)
+	_tween.tween_callback(func(): _collect_point(_pos))
 	_tween.tween_callback(func(): _tween = null)
 
 
+func _collect_point(pos : Vector2i):
+	if tile_map.get_cell_tile_data(POINT, pos) != null:
+		tile_map.erase_cell(POINT, pos)	
+		_play_collect_sound()
 
 
+func _play_collect_sound():
+	if audio.playing == false:
+		audio.stream = coins_sound.pick_random();
+		audio.play()
